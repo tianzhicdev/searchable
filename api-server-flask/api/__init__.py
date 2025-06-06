16cbfd36dfb6538cd18927394151d1cf3860f95c
tianzhicdev <tianzhic.dev@gmail.com>
2025-06-06 11:28:38 -0400

    no v1_kv file

--- api-server-flask/api/__init__.py
+++ api-server-flask/api/__init__.py
@@ -25,7 +25,7 @@ from .searchable_routes import *
 from .searchable_v1 import *
 from .searchable_v1_no_auth import *
 from .searchable_v1_withdrawal import *
-from .searchable_v1_kv import *
+# from .searchable_v1_kv import *
 from .searchable_v1_files import *
 # Setup database
 @app.before_first_request
