cd29304a8d70d6b08770d156eb701ac985257db2
tianzhicdev <tianzhic.dev@gmail.com>
2025-06-30 12:35:41 -0400

    chaotic

--- frontend/src/ui-component/Logo.js
+++ frontend/src/ui-component/Logo.js
@@ -7,6 +7,8 @@ import config from '../config';
 // Import logo images
 import camelLogo from './../assets/images/camel_logo.gif';
 import eccentricLogo from './../assets/images/eccentricprotocol.gif';
+import abitchaoticLogo from './../assets/images/abitchaotic.gif';
+import { Typography } from '@material-ui/core';
 /**
  * if you want to use image instead of <svg> uncomment following.
  *
@@ -23,20 +25,37 @@ const Logo = () => {
     const getLogo = () => {
         if (config.APP_BRANDING === 'silkroadonlightning') {
             return camelLogo;
-        } else {
+
+        } else if (config.APP_BRANDING === 'eccentricprotocol') {
             return eccentricLogo;
+        } else if (config.APP_BRANDING === 'abitchaotic') {
+            return abitchaoticLogo; // Use the eccentric logo for local branding in dark mode
+        } else {
+            return abitchaoticLogo; // Use the same logo for local branding
         }
     };
     
     return (
-        <img 
-            src={getLogo()} 
-            alt={config.APP_BRANDING === 'eccentricprotocol' ? 'Eccentric Protocol' : 'Silk Road on Lightning'} 
-            width="100%" 
-            style={{ 
-                maxWidth: '320px'
-            }} 
-        />
+        <div style={{ textAlign: 'center' }}>
+            <img 
+                src={getLogo()} 
+                alt={config.APP_BRANDING === 'eccentricprotocol' ? 'Eccentric Protocol' : 'Silk Road on Lightning'} 
+                width="100%" 
+                style={{ 
+                    maxWidth: '320px'
+                }} 
+            />
+            {(config.APP_BRANDING !== 'silkroadonlightning' && config.APP_BRANDING !== 'eccentricprotocol') && (
+                <div>
+                <Typography variant="h1" >
+                    warp to the future 
+                </Typography>
+                <Typography variant="h2">
+                    it is a bit chaotic
+                </Typography>
+                </div>
+            )}
+        </div>
     );
 };
 
